f082747e9f4a518ccceb3779575aecb3113ef199
GalaxyIgor <igornbelisario@gmail.com>
2024-09-05 22:20:26 -0300

    Atualização

--- For/Repetidor com for.cpp
+++ For/Repetidor com for.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	int i; //contador
-	int n; //quantidade de repetição
+	int n; //quantidade de repetiï¿½ï¿½o
 	cin >> n; //numero a ser inserido(entrada)
 	for(i =0; i< n; i++) //mostando a mensagen n vezes
 	cout<< i + 1<< "- " <<"Vou ser um bom menino daqui pra frente"<< endl; //frase a ser repetida n vezes( saida)
